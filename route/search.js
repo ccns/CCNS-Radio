93de6eda319638e1d3f0c53f65a3a6b6ae2374cd
Daniel Chen <team6612@gmail.com>
2020-04-12 01:53:22 -0700

    Fix search api

--- route/search.js
+++ route/search.js
@@ -1,12 +1,11 @@
-const request = require('request')
+const YoutubeVideoFetcher = require('../lib/fetcher.js')
 const express = require('express')
 const bodyParser = require('body-parser')
 
 class SearchRouter {
-  constructor (api_key) {
+  constructor (config) {
     this.router = express.Router()
-    this.api_key = api_key
-    this.base_url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=' + this.api_key
+    this.fetcher = new YoutubeVideoFetcher(config)
 
     this.router.use(bodyParser.urlencoded({ extended: false }))
     this.router.use(bodyParser.json())
@@ -17,7 +16,7 @@ class SearchRouter {
       var q = req.body.q
       var pageToken = req.body.pageToken
 
-      self.search(q, pageToken).then(function (data) {
+      self.fetcher.search(q, pageToken).then(function (data) {
         res.json(data)
       }).catch(function (err) {
         res.send('Failed')
@@ -27,44 +26,6 @@ class SearchRouter {
 
   getRouter () {
     return this.router
-  }
-
-  search (query, pageToken) {
-    var q = query
-    var maxResults = 25
-    var type = 'video,playlist'
-
-    var url = this.base_url +
-              '&q=' + encodeURIComponent(query) +
-              '&maxResults=' + maxResults +
-              '&type=' + type
-
-    if (pageToken != undefined) {
-      url += '&pageToken=' + pageToken
-    }
-
-    return new Promise(function (resolve, reject) {
-      request(url, function (err, res, body) {
-        if (!err && res.statusCode == 200) {
-          var body = JSON.parse(body)
-          var items = body.items
-          var nextPageToken = body.nextPageToken
-          var prevPageToken = body.prevPageToken
-          items = items.map(function (item) {
-            return {
-              id: item.id.videoId,
-              title: item.snippet.title,
-              url: 'https://youtu.be/' + item.id.videoId,
-              thumbnail: item.snippet.thumbnails.default
-            }
-          })
-          resolve({items: items, nextPageToken: nextPageToken, prevPageToken: prevPageToken})
-        } else {
-          console.log('[error] Search failed.')
-          reject()
-        }
-      })
-    })
   }
 }
 
